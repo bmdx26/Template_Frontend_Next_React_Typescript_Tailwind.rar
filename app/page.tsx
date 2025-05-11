4ef80c18fdfef86e9c68d3b61d17a9a32666844f
bmdx26 <poseidonx26@gmail.com>
2025-05-11 11:25:02 +0530

    template

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex justify-center items-end h-screen">
-      <p className="absolute bottom-5 text-center text-black helvetica-font ">bhoumik eugene</p>
+    <div>
+      
     </div>
   );
 }
